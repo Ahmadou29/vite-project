b0a24a56283742d297f12f28646416dc6296ce1c
Nogaye Ndao <nogayendao2020@gmail.com>
2025-07-21 00:59:51 -1200

    Ajout du Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Detail from './components/Detail.jsx'; 
 import './App.css';
 
 function App() {
-
   return (
-    <>
     <Routes>
-        
+      <Route path="/" element={<Detail />} />
     </Routes>
-    </>
   );
 }
 
