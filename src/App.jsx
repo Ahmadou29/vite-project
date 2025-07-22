9c153baf52d08e511a70afaedb6dbbe3de389430
amadoudiarroudiallo <amadoudiarroudiallo203@email.comgit config --global user.email amadoudiarroudiallo203@gmail.com>
2025-07-22 16:57:22 +0000

    jjj

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
+/** @format */
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Blog from './components/Blog';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import { CartProvider } from './components/CartContext';
+import ProductDetail from './components/ProductDetail';
+// import Blog from './components/Blog';
 
 function App() {
-
   return (
-    <>
-    
-        < Blog />    
-        </>
+    <div>
+      <CartProvider>
+        <Navbar />
+
+        <Routes>
+          <Route path="/blog" element={<Blog />} />
+          {/* <Route path="/detail/:blog/:subBlog/:id" element={<ProductDetail />} /> */}
+        </Routes>
+        <Footer />
+      </CartProvider>
+    </div>
   );
 }
 
