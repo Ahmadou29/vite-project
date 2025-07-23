75014ddd7ae9f4b9b9becf01a7026137f8a8f75f
aicha1902 <lyaissata1902@gmail.com>
2025-07-23 15:26:25 +0200

    commit1 url photo

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@
 
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./components/login";
+import Login from "./components/Login";
 import AdminDashboard from "./components/AdminDashboard";
 // import ProtectedRoute from "./components/ProtectedRoute";
 
