7dc52cca36e18cb36a436a66f8ad0b43ee99c3d7
aicha1902 <lyaissata1902@gmail.com>
2025-07-23 13:01:02 +0200

    my first commit blog2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+ import TabsShop from './components/TabsShop';
 
 function App() {
 
   return (
     <>
     <Routes>
-        
+        <Route path='/' element={<TabsShop />}/>
     </Routes>
-    </>
-  );
-}
+     </>
+   );
+ }
 
-export default App;
+ export default App;
+
+// import React from "react";
+// import TabsShop from "./TabsShop";
+
+// function App() {
+//   return <TabsShop />;
+  
+// }
+
+// export default App;
