3bc77e081ece8db250ced247e7193b58490d9147
amadoudiarroudiallo <amadoudiarroudiallo203@email.comgit config --global user.email amadoudiarroudiallo203@gmail.com>
2025-07-24 23:48:21 +0000

    modif image

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-import accessImage from '../assets/image/kids-accessories-gallery/access1.jpg';
+// import accessImage from '../assets/image/kids-accessories-gallery/access1.jpg';
 
 const Detail = () => {
   const [quantity, setQuantity] = useState(1);
